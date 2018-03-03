26ca37ac8d932a211360797ff864dc06b0721671
Administrator <Administrator@USER-20170416TD>
2018-03-03 17:12:22 +0800

    async

---
+++ Enumerator/Friend.cs
@@ -0,0 +1,90 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Enumerator
+//{
+//   public class Friend
+//    {
+//        private string name { get; set; }
+//        public string Name
+//        {
+//            get { return name; }
+//            set { name = value; }
+//        }
+//        public Friend(string name)
+//        {
+//            this.name = name;
+//        }
+//    }
+//    //IEnumerator
+//    public class Friends : IEnumerable
+//    {
+//        private Friend[] friendarray;
+//        public Friends()
+//        {
+//            friendarray = new Friend[]
+//            {
+//                new Friend("彭文芳"),
+//                new Friend("李玲玲"),
+//                new Friend("陆峰")
+//            };
+//        }
+//        /// <summary>
+//        /// 索引器
+//        /// </summary>
+//        /// <param name="index"></param>
+//        /// <returns></returns>
+//        public Friend this[int index]
+//        {
+//            get { return friendarray[index]; }
+
+//        }
+//        public int Count
+//        {
+//            get { return friendarray.Length; }
+//        }
+//        public IEnumerator<Friend> GetEnumerator()
+//        {
+//            return new FriendIterator<Friend>(this);
+//        }
+//    }
+//    public class FriendIterator<Friend> :IEnumerator<Friend>
+//    {
+//        private readonly Friends friends;
+//        private int index;
+//        private Friend current;
+//        internal FriendIterator(Friends friendcollection)
+//        {
+//            this.friends = friendcollection;
+//        }
+//        public Friend Current
+//        {
+//            get
+//            {
+//                return this.current;
+//            }
+//        }
+//        public bool MoveNext()
+//        {
+//            if (index + 1 > friends.Count)
+//            {
+//                return false;
+//            }
+//            else
+//            {
+//                this.current = friends[index];
+//                index++;
+//                return true;
+//            }
+//        }
+//        public void Reset()
+//        {
+//            index = 0;
+//        }
+
+//        public void Dispose() { }
+//    }
+//}
---
+++ Enumerator/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+
+
+
+            List<object> listobject = new List<object>();
+
+            List<string> liststr = new List<string>();
+
+            //IEnumerable<out T>  out 关键子标识
+            //协变的过程 （协变指的是泛型的类型参数可以从一个派生类隐式转换成基类）
+            listobject.AddRange(liststr);
+
+
+            IComparer<object> objectComparer = new TestComparer();
+
+            //逆变的过程（逆变指的是泛型类型参数可以从一个基类隐式转换成派生类）  in关键子标识
+            IComparer<string> stringComparer = new TestComparer();
+
+            liststr.Sort(objectComparer);
+
+            //listobject.Sort(stringComparer);
+
+        }
+    }
+    
+    public class TestComparer : IComparer<object>
+    {
+        public int Compare(object obj1, object obj2)
+        {
+            return obj1.ToString().CompareTo(obj2.ToString());
+        }
+    }
+}
